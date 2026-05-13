a3097966c6986584eeca4e254c0c72eee57cb56e
CtrlByBhagya <mishu.08nov23@gmail.com>
2026-05-13 22:59:38 +0530

    Added NestedLoop Code

---
+++ NestedLoop.py
@@ -0,0 +1,10 @@
+username= input("enter username:")
+password=input("enter password:")
+
+if username =="bhagyashree":
+    if password == "1234":
+        print("Login Successfully !!")
+    else:
+        print("Wrong Password")
+else:
+    print("Wrong Username")            
