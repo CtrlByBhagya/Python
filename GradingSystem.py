75803e7729090fa0e3e1559ae58d013d42049577
CtrlByBhagya <mishu.08nov23@gmail.com>
2026-05-13 22:33:48 +0530

    Added GradingSystem Code

---
+++ GradingSystem.py
@@ -0,0 +1,15 @@
+a=int(input("enter your marks:"))
+if (a>=90 and a<100):
+    print("grade A+")
+elif (a>=80 and a<90):
+    print("grade A")
+elif (a>=70 and a<80):
+    print("grade B")
+elif (a>=60 and a<70):
+    print("grade C")
+elif (a>=50 and a<60):
+    print("grade D")
+elif (a>=40 and a<50):
+    print("grade E")
+else:
+    print("grade F")                        
