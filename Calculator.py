734a440f1b43c90bf4fe710560381c6cd2269f4d
CtrlByBhagya <mishu.08nov23@gmail.com>
2026-05-28 23:42:38 +0530

    Added Calculator Code

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a=int(input("enter first number:"))
+operator=str(input("enter opetrator:"))        
+b=int(input("enter second number:"))
+if operator=="+":
+    print(a+b)
+if operator=="-":
+    print(a-b)
+if operator=="*":
+    print(a*b)
+if operator=="/":
+    print(a/b)
+                
